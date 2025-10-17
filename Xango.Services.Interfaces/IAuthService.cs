b96281aea413c371bb7d3de6382e3e9e5ca48e46
meritumenterprises-netizen <meritumenterprises@gmail.com>
2025-10-17 18:44:12 +0200

    Minor change

--- Xango.Services.Interfaces/IAuthService.cs
+++ Xango.Services.Interfaces/IAuthService.cs
@@ -11,7 +11,7 @@ namespace Xango.Services.Interfaces
 
         Task<ResponseDto> GetUser(string email);
         Task<ResponseDto> GetUserById(string id);
-
+         
         Task<ResponseDto?> Logout();
         
     }
